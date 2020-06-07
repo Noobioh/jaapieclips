9d141973203e2fb9bf388eb1f39b56e2f4d16510
Noobioh <jaap.naayer@outlook.com>
2020-06-07 21:31:21 +0200

    Getvideos React

--- jaapieclips/ClientApp/src/components/Videos.js
+++ jaapieclips/ClientApp/src/components/Videos.js
@@ -4,8 +4,33 @@ import './videos.css';
 
 
 export default class Videos extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { videos: [], loading: true };
+      }
+
+      componentDidMount() {
+        this.getVideos();
+      }
+
     render() {
+
+            let contents = this.state.loading
+              ? <p><em>Loading...</em></p>
+              :   <div>
+                  {videos.map(video =>
+                  <a href={"/getvideos/getvideocontent?path="+video.path}> 
+                 <Card id="videocard">
+                 <CardTitle>{video.name}</CardTitle>
+                 <CardSubtitle>{video.description}</CardSubtitle>
+                 <CardImg src="./thumbnails/mw.jpg"></CardImg>
+             </Card>
+             </a>
+                   )}
+                </div>
         return (
+
             <Container fluid>
                     <Row>
                         <Col>
@@ -14,44 +39,23 @@ export default class Videos extends Component {
                     </Row>
                     <Row>
                         <Col>
+                        <a href="/getvideos/getvideocontent">
                         <Card id="videocard">
                             <CardTitle>Modern Warfare</CardTitle>
                             <CardSubtitle>Warzone clip</CardSubtitle>
                             <CardImg src="./thumbnails/mw.jpg"></CardImg>
                         </Card>
-                        <Card id="videocard">
-                            <CardTitle>Modern Warfare</CardTitle>
-                            <CardSubtitle>Warzone clip</CardSubtitle>
-                            <CardImg src="./thumbnails/mw.jpg"></CardImg>
-                        </Card>
-                        <Card id="videocard">
-                            <CardTitle>Modern Warfare</CardTitle>
-                            <CardSubtitle>Warzone clip</CardSubtitle>
-                            <CardImg src="./thumbnails/mw.jpg"></CardImg>
-                        </Card>
-                        <Card id="videocard">
-                            <CardTitle>Modern Warfare</CardTitle>
-                            <CardSubtitle>Warzone clip</CardSubtitle>
-                            <CardImg src="./thumbnails/mw.jpg"></CardImg>
-                        </Card>
-                        <Card id="videocard">
-                            <CardTitle>Modern Warfare</CardTitle>
-                            <CardSubtitle>Warzone clip</CardSubtitle>
-                            <CardImg src="./thumbnails/mw.jpg"></CardImg>
-                        </Card>
-                        <Card id="videocard">
-                            <CardTitle>Modern Warfare</CardTitle>
-                            <CardSubtitle>Warzone clip</CardSubtitle>
-                            <CardImg src="./thumbnails/mw.jpg"></CardImg>
-                        </Card>
-                        <Card id="videocard">
-                            <CardTitle>Modern Warfare</CardTitle>
-                            <CardSubtitle>Warzone clip</CardSubtitle>
-                            <CardImg src="./thumbnails/mw.jpg"></CardImg>
-                        </Card>
+                        </a>
                         </Col>
                     </Row>
             </Container>
         )
     }
+
+    async getVideos() {
+        const response = await fetch('getvideos/videos');
+        const data = await response.json(); 
+        this.setState({ videos: data, loading: false });
+      }
+
 }
